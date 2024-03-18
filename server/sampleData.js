c0de7a5981a19ea15224008a58742da103a48791
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-03-18 19:41:00 +0530

    + sample Data

---
+++ server/sampleData.js
@@ -0,0 +1,94 @@
+// Projects
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Tony',
+      lastName: 'Stark',
+      email: 'ironman@gmail.com',
+      phone: '343-567-4333',
+      state: 'California',
+      country: 'USA',
+    },
+    {
+      id: '2',
+      name: 'Natasha',
+      lastName: 'Romanoff',
+      email: 'blackwidow@gmail.com',
+      phone: '223-567-3322',
+      state: 'New York',
+      country: 'USA',
+    },
+    {
+      id: '3',
+      name: 'Thor',
+      lastName: 'Odinson',
+      email: 'thor@gmail.com',
+      phone: '324-331-4333',
+      state: 'washington',
+      country: 'USA',
+    },
+    {
+      id: '4',
+      name: 'Steve',
+      lastName: 'Rogers',
+      email: 'steve@gmail.com',
+      phone: '344-562-6787',
+      state: 'New York',
+      country: 'USA',
+    },
+    {
+      id: '5',
+      name: 'Bruce',
+      lastName: 'Banner',
+      email: 'bruce@gmail.com',
+      phone: '321-468-8887',
+      state: 'michigan',
+      country: 'USA',
+    },
+  ];
+  
+  module.exports = { projects, clients };
