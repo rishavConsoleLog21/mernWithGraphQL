167a2e94c81c4b45211b2d34095868a3a548fcfe
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-03-19 15:11:55 +0530

    + Frontend(Vite)

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>
+          Welcome to the <span>React</span> App
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
