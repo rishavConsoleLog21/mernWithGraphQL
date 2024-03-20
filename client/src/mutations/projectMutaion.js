6e92b5c03da6fd07a43960858e9ff1c72a87178f
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-03-20 17:23:23 +0530

    + To Add New Project

---
+++ client/src/mutations/projectMutaion.js
@@ -0,0 +1,38 @@
+import { gql } from "@apollo/client";
+
+const DELETE_PROJECT = gql`
+  mutation DeleteProject($id: ID!) {
+    deleteProject(id: $id) {
+      id
+    }
+  }
+`;
+
+const ADD_PROJECT = gql`
+  mutation AddProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
+      id
+      name
+      description
+      status
+      client {
+        id
+        name
+        email
+        phone
+      }
+    }
+  }
+`;
+
+export { DELETE_PROJECT, ADD_PROJECT };
---
+++ client/src/pages/Project.jsx
@@ -0,0 +1,39 @@
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import Snipper from "../components/Spinner";
+import { GET_PROJECT } from "../queries/projectQueries";
+import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
+
+const Project = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: { id },
+  });
+
+  if (loading) return <Snipper />;
+  if (error) return <p>Somethings Went Wrong</p>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link
+            to="/"
+            className="btn btn-secondary btn-sm w-25 d-inline ms-auto"
+          >
+            Back
+          </Link>
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+          <h5 className="mt-3">Project Status</h5>
+          <p className="lead">{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
+          <DeleteProjectButton projectId={data.project.id} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Project;
