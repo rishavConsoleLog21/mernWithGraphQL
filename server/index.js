073ac00cb32e50cc7ffbd6a21168e18e66dddee6
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-03-22 12:48:03 +0530

    Fixing for Production

--- server/index.js
+++ server/index.js
@@ -23,11 +23,11 @@ app.use(
 
 // Serve client
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+ app.use(express.static('public'))
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "client", "dist", "index.html")
+      path.resolve(__dirname, "../", "public", "index.html")
     )
   );
 }
